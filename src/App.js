fdd836794a6062daa1404bfb225368480547ea81
Jacques Ramphal <jacques.r@myplanet.com>
2020-10-08 16:25:18 -0400

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 // App.jsx
  
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom'; 
 import logo from './i/logo.svg';
-import Projects from './Projects.js';  
+import Thumbnail from './Thumbnail.js';
+
 import './App.css';
  
 function App() {
   return (
+    <BrowserRouter>
       <div className="App">
         
         <div className="navigation">
@@ -19,7 +22,32 @@ function App() {
   
           </div>
         </div>
+        <div>
+          <h1>Projects</h1>
+          <Thumbnail
+            link="/twitter"
+            image="http://twitter-image-url.jpg"
+            title="Twitter Newsfeed"
+            category="Mobile App"
+          />
+          
+          <Thumbnail
+            link="/airbnb"
+            image="http://airbnb-image-url.jpg>"
+            title="Airbnb Experiences"
+            category="Website"
+          />
+          
+          <Thumbnail
+            link="/photoshop"
+            image="http://photoshop-image-url.jpg"
+            title="Photoshop Redesign"
+            category="Desktop App"
+          />
+        </div>
       </div>
+    </BrowserRouter>
+
   );
 }
  
